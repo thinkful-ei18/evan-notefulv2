bed30d6212bb0bc189fe2916ec15494adc2bb6a1
Evan Garrett <evang522@gmail.com>
2018-02-06 16:14:16 -0500

    implement knex commands for deletes, beginning to import to notes.router.js

---
+++ scratch/notes-queries.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const knex = require('../knex');
+
+// const searchTerm = null;
+
+if (searchTerm) {
+  knex
+    .select('title','content','id')
+    .from('notes')
+    .where(`${searchTerm}`)
+    .then((response) => {
+      console.log(response);
+    });
+} else {
+  knex
+    .select('title','content','id')
+    .from('notes')
+    .then((response) => {
+      console.log(response);
+    });
+}
+
+// const id = '1001';
+// knex
+//   .select('title','content','id')
+//   .from('notes')
+//   .where('id',`${id}`)
+//   .then((response) => {
+//     console.log(response[0].title);
+//   });
+
+
+// const noteID = '1004';
+// const updateObj = 
+// {title:'testing 987'};
+
+// knex('notes')
+//   .where('id',`${noteID}`)
+//   .update(updateObj)
+//   .then((response) => {
+//     console.log(response);
+//   }); 
+
+// router.post('/notes', (req, res, next) => {
+//   const { title, content } = req.body;
+  
+//   const newItem = { title, content };
+//   /***** Never trust users - validate input *****/
+//   if (!newItem.title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err);
+//   }
+//   router.post('/notes', (req, res, next) => {
+//     const { title, content } = req.body;
+    
+//     const newItem = { title, content };
+//     /***** Never trust users - validate input *****/
+//     if (!newItem.title) {
+//       const err = new Error('Missing `title` in request body');
+//       err.status = 400;
+//       return next(err);
+//     }
+  
+//     /*
+//     notes.create(newItem)
+//       .then(item => {
+//         if (item) {
+//           res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+//         } 
+//       })
+//       .catch(err => next(err));
+//     */
+//   });
+  
+
+// const newItem = {title:'testing 12353247', content:'I have three jellyfish'};
+
+// knex('notes')
+//   .insert(newItem)
+//   .then((response) => {
+//     console.log(response);
+//   }); 
+
+
+// router.delete('/notes/:id', (req, res, next) => {
+//   const id = req.params.id;
+  
+//   /*
+//   notes.delete(id)
+//     .then(count => {
+//       if (count) {
+//         res.status(204).end();
+//       } else {
+//         next();
+//       }
+//     })
+//     .catch(err => next(err));
+//   */
+// });
+
+// const id = 1005;
+// knex('notes')
+//   .where('id',`${id}`)
+//   .del()
+//   .then((results) => {
+//     console.log(results);
+//   });
+
+
+
+knex.destroy();
+
---
+++ scratch/sqlops.sql
@@ -0,0 +1,26 @@
+-- Notes table has name, title, id. 
+
+DROP TABLE IF EXISTS notes;
+
+CREATE TABLE notes(
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  content text NOT NULL,
+  created timestamp DEFAULT now()
+);
+
+ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
+
+INSERT INTO notes 
+(title,content) VALUES
+('test note',' Hello World!') RETURNING id, title;
+
+
+INSERT INTO notes 
+  (title,content) VALUES
+  ('Cats are Cool','Dogs do Drool'),
+  ('Cats are fat','Dogs do not Drool'),
+  ('Cats are fuzzy','Dogs Drool on sundays'),
+  ('Cats are cute','Dogs Drool tomorrow'),
+  ('Cats are sometimes Fat','Dogs Drool yesterday'),
+  ('Cats are Sometimes Orange','Dogs Drool 5') RETURNING id;
